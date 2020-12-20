a809bdf758bd2a0a8b61a3db2130563d1b4e2261
ilya fadeev <ilya-artdesigner@yandex.ru>
2020-12-20 14:18:44 +0300

    modal-video: done

--- app/js/main.js
+++ app/js/main.js
@@ -22,3 +22,22 @@ const phone_mask = Maska.create(tel, {
   tokens: { '#': { pattern: /[0-9]/ }},
   mask: '+# (###) ### ## ##',
 });
+
+//modal video
+
+const open = document.querySelector('.cta__link-video');
+const close = document.querySelector('.modal__box__inner-closer');
+const modal = document.querySelector('.modal__box');
+
+open.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+// VIDEO PLAYER
+
+const player = new Plyr(document.querySelector('#player'));
